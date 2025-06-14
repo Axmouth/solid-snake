69a7e25fce3af3224c288eb215f1c3afa9759b4c
aggronfg@yahoo.com <aggronfg@yahoo.com>
2025-06-14 22:27:52 +0300

    Start implementing github pages

--- solid-snake-vm/src/bin/docgen.rs
+++ solid-snake-vm/src/bin/docgen.rs
@@ -10,21 +10,21 @@ fn main() -> std::io::Result<()> {
     let mut md_file = File::create("./solid-snake-vm/INSTRUCTIONS.md")?;
     md_file.write_all(docs.to_markdown().as_bytes())?;
 
-    let mut json_file = File::create("./solid-snake-vm/docs.json")?;
+    let mut json_file = File::create("./docs/instructions/docs.json")?;
     json_file.write_all(docs.to_json().as_bytes())?;
 
     println!("Documentation generated successfully in INSTRUCTIONS.md and docs.json");
 
-    let template = fs::read_to_string("./solid-snake-vm/docs.template.html").expect("Failed to read template.html");
-    let docs = fs::read_to_string("./solid-snake-vm/docs.json").expect("Failed to read docs.json");
+    let template = fs::read_to_string("./solid-snake-vm/instructions.docs.template.html").expect("Failed to read template.html");
+    let docs = fs::read_to_string("./docs/instructions/docs.json").expect("Failed to read docs.json");
 
     // Escape closing script tags to prevent breaking out of the tag
     let safe_json = docs.replace("</script>", "<\\/script>");
 
     let output = template.replace("__DOCS_PLACEHOLDER__", &safe_json);
-    fs::write("./solid-snake-vm/docs.index.html", output).expect("Failed to write docs.index.html");
+    fs::write("./docs/instructions/index.html", output).expect("Failed to write instructions/index.html");
 
-    println!("✅ Generated docs.index.html");
+    println!("✅ Generated instructions.html");
 
     Ok(())
 }
